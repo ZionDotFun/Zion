6c34b5038136d71b531a074f4b952d3ca9181da2
Muhammed Talha Korkmaz <kriptovekediler@gmail.com>
2024-12-18 01:16:02 +0300

    fetch price

--- zion/zion/zion/zion/zion/zion/tools/get_balance.py
+++ zion/zion/zion/zion/zion/zion/tools/get_balance.py
@@ -3,6 +3,7 @@ from solders.pubkey import Pubkey
 from solana.rpc.commitment import Confirmed
 from solana.rpc.types import TokenAccountOpts
 from solana.rpc.async_api import AsyncClient
+from solders.keypair import Keypair
 from solders.rpc.responses import GetTokenAccountsByOwnerResp
 from spl.token.constants import LAMPORTS_PER_SOL
 from agentipy.agent import SolanaAgentKit
