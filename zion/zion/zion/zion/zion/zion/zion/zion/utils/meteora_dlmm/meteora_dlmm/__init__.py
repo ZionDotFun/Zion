3f8fb65d31d8be496f0d0ac5cf65ba272feb1911
Muhammed Talha Korkmaz <103596213+tkorkmazeth@users.noreply.github.com>
2025-01-03 00:30:03 +0300

    Merge: Merge pull request #29 from niceberginc/use_pyth
    feat: pyth_price_feed

--- zion/zion/zion/zion/zion/zion/zion/zion/utils/meteora_dlmm/meteora_dlmm/__init__.py
+++ zion/zion/zion/zion/zion/zion/zion/zion/utils/meteora_dlmm/meteora_dlmm/__init__.py
@@ -1,5 +1,3 @@
-import json
-import os
 from typing import Optional
 
 from anchorpy import Program
@@ -14,6 +12,7 @@ from spl.token.constants import TOKEN_PROGRAM_ID
 from spl.token.instructions import get_associated_token_address
 
 from .constants import LBCLMM_PROGRAM_IDS
+from .idl import IDL
 from .utils import (bin_id_to_bin_array_index,
                     compute_base_factor_from_fee_bps,
                     derive_bin_array_bitmap_extension,
@@ -34,24 +33,10 @@ async def create_customizable_permissionless_lb_pair(
     activation_point: Optional[int] = None,
     opt: Optional[dict] = None,
 ):
-    """
-    Creates a customizable permissionless LB pair.
-    """
-
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    json_path = os.path.join(current_dir, 'idl.json')
-
-    try:
-        with open(json_path, 'r') as json_file:
-            idl_data = json.load(json_file)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"IDL file not found at {json_path}")
-    except json.JSONDecodeError as e:
-        raise ValueError(f"Error decoding JSON from {json_path}: {e}")
-
+    """Creates a customizable permissionless LB pair."""
     provider = AnchorProvider(connection, {}, anchor_provider.DEFAULT_OPTIONS)
     program_id = opt.get("program_id") if opt else LBCLMM_PROGRAM_IDS[opt.get("cluster")]
-    program = Program(idl_data, program_id, provider)
+    program = Program(IDL, program_id, provider)
 
     lb_pair, _ = derive_customizable_permissionless_lb_pair(token_x, token_y, program.program_id)
     reserve_x, _ = derive_reserve(token_x, lb_pair, program.program_id)
@@ -64,6 +49,7 @@ async def create_customizable_permissionless_lb_pair(
         if is_overflow_default_bin_array_bitmap(active_bin_array_index)
         else None
     )
+
     ix_data = Container(
         activeId=active_id,
         binStep=bin_step,
