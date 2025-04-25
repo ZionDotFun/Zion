f4f0463c6395094f5289d7128088b73ba97a092b
Alhibb <63309522+Alhibb@users.noreply.github.com>
2025-04-25 15:10:51 +0100

    Updated README

--- zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/agentipy/__init__.py
+++ zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/zion/agentipy/__init__.py
@@ -2,9 +2,7 @@ import logging
 
 from agentipy.agent import SolanaAgentKit
 from agentipy.langchain import create_solana_tools
-from agentipy.mcp.all_actions import ALL_ACTIONS
-from agentipy.mcp.mcp_server import start_mcp_server
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-__all__ = ["SolanaAgentKit", "create_solana_tools","start_mcp_server", "ALL_ACTIONS"]
+__all__ = ["SolanaAgentKit", "create_solana_tools"]
